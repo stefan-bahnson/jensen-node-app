f670fa9ea5a7bb0abaf20aaab159b78f5d284798
stefan.bahnson <stefan.bahnson@findwise.com>
2016-12-09 09:09:20 +0100

    update mock db

--- db/mockDB.js
+++ db/mockDB.js
@@ -1,22 +1,22 @@
 var postsDB = [
   {
     id: 1,
-    title: 'Lena Haxxor',
-    description: 'Breaking news! Lena learns to code awesome node apps! :)',
-    date: '2016-11-28'
+    title: 'Blog post with id 1',
+    content: 'Content text for blog post with id 1',
+    date: '2016-12-07'
   },
   {
     id: 2,
-    title: 'Stefan gillar Angular!',
-    description: 'WTF!',
-    date: '2016-11-28'
+    title: 'Blog post with id 2',
+    content: 'Content text for blog post with id 2',
+    date: '2016-12-08'
   },
   {
     id: 3,
-    title: 'Eldorados köttbullar, godare än Mamma Scans',
-    description: 'Va fan dillar du om!',
-    date: '2016-11-28'
-  }
+    title: 'Blog post with id 2',
+    content: 'Content text for blog post with id 2',
+    date: '2016-12-09'
+  },
 ];
 
 module.exports = postsDB;
